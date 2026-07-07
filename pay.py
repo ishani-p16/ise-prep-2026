0e2f6945a772ae3b4db4d05ac3b9aab7d9ce4ea6
ishani-p16 <ishanipraseed25@gmail.com>
2026-07-07 15:09:12 +0530

    Add files via upload

---
+++ pay.py
@@ -0,0 +1,5 @@
+#Pay calculator-PY4E chap2
+hours=float(input('enter your working hours:'))
+rate=float(input('enter your rate:'))
+pay=hours*rate
+print('Weekly pay is:',pay)
---
+++ temp.py
@@ -0,0 +1,4 @@
+#Converting celsius to fahrenheit
+c=float(input('Enter the temp in celsius:'))
+f=c*9/5+32
+print('the temp in fahrenheit is:',f)
